3b4f9b7f8cb0c8af10835c3a19a6775d0a9b623d
Vandim87 <107519365+Vandim87@users.noreply.github.com>
2022-07-15 23:53:15 -0300

    13

--- Js/login.js
+++ Js/login.js
@@ -1,21 +1,22 @@
-const input = document.querSelector('.login_input');
-const button = document.querSelector('.login_button');
-const form = document.querySelector('.login-form')
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
 
-const validateInput = ({target}) => 
-{
-if (target.value.length > 2)
-   {button.removeAttribute('disabled')}   
-else
-   {button.setAttribute('disabled', '');} 
+
+const validateInput = ({ target }) =>{
+   if (target.value.length > 2) {
+      button.removeAttribute('disabled');
+      return;
+   }
+   button.setAttribute('disabled', '');
 }
 
-const handleSubmit = (Event) => 
-{
-Event.preventDefault();
-localStorage.setItem('player', input.value);
-window.location = 'pages/game.html';
+const handleSubmit = (event) => {
+   event.preventDefault();
+   localStorage.setItem('player', input.value);
+   window.location = 'pages/game.html';
 }
 
 input.addEventListener('input', validateInput);
-form.addEventListener('suubmit', handleSubmit);
+form.addEventListener('submit', handleSubmit);
+
